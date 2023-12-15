cd3d8f4fb186fbcc2891c2ff531aac67d977a575
Milutin Dzunic <milutin.dzunic@localsearch.ch>
2023-12-15 13:22:40 +0100

    Day 15

---
+++ advent-of-typescript/15-box-toys.ts
@@ -0,0 +1,28 @@
+import { Expect, Equal } from "type-testing";
+
+// type definition
+type BoxToys<
+  Toy extends string,
+  Num extends number,
+  Acc extends string[] = never,
+  Cnt extends Toy[] = []
+> = Cnt["length"] extends Num
+  ? BoxToys<Toy, Num, Acc | Cnt, [...Cnt, Toy]> // current number is in the union
+  : Acc["length"] extends 0
+  ? BoxToys<Toy, Num, Acc, [...Cnt, Toy]>
+  : Acc; // current number is not in the union, check if we have finished or we have yet to add to the accumulator
+
+// tests
+type test_doll_actual = BoxToys<"doll", 1>;
+//   ^?
+type test_doll_expected = ["doll"];
+type test_doll = Expect<Equal<test_doll_expected, test_doll_actual>>;
+
+type test_nutcracker_actual = BoxToys<"nutcracker", 3 | 4>;
+//   ^?
+type test_nutcracker_expected =
+  | ["nutcracker", "nutcracker", "nutcracker"]
+  | ["nutcracker", "nutcracker", "nutcracker", "nutcracker"];
+type test_nutcracker = Expect<
+  Equal<test_nutcracker_expected, test_nutcracker_actual>
+>;
