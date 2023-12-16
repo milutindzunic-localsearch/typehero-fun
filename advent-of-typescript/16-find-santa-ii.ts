5662928da868baf2fea9ea0cb58d035cdfab818a
Milutin Dzunic <milutin.dzunic@localsearch.ch>
2023-12-16 12:58:40 +0100

    Day 16

---
+++ advent-of-typescript/16-find-santa-ii.ts
@@ -0,0 +1,70 @@
+import { Expect, Equal } from 'type-testing';
+
+// type definitions
+type Santa = "🎅🏼";
+// type taken from day 12
+type FindSantaInRow<T extends unknown[], Cnt extends any[] = []> =
+ T extends [infer First, ...infer Rest] ? // check the first element
+ First extends Santa ? Cnt["length"] // santa found
+ : FindSantaInRow<Rest, [...Cnt, 0]> // keep looking
+ : never; // no santa found
+type FindSanta<F extends unknown[][], Row extends any[] = []> =
+  F["length"] extends Row["length"] ? never : // end of recursion, santa not found
+	FindSantaInRow<F[Row["length"]]> extends never ? FindSanta<F, [...Row, 0]> : // santa not here, go to next row
+	[Row["length"], FindSantaInRow<F[Row["length"]]>] // santa found in this row
+	;
+
+type Forest0 = [
+  ['🎅🏼', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+];
+type test_0_actual = FindSanta<Forest0>;
+//   ^?
+type test_0_expected = [0, 0];
+type test_0 = Expect<Equal<test_0_expected, test_0_actual>>;
+
+type Forest1 = [
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎅🏼', '🎄', '🎄'],
+];
+type test_1_actual = FindSanta<Forest1>;
+//   ^?
+type test_1_expected = [3, 1];
+type test_1 = Expect<Equal<test_1_expected, test_1_actual>>;
+
+type Forest2 = [
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎅🏼', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+];
+type test_2_actual = FindSanta<Forest2>;
+//   ^?
+type test_2_expected = [2, 2];
+type test_2 = Expect<Equal<test_2_expected, test_2_actual>>;
+
+type Forest3 = [
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎅🏼', '🎄', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+];
+type test_3_actual = FindSanta<Forest3>;
+//   ^?
+type test_3_expected = [2, 1];
+type test_3 = Expect<Equal<test_3_expected, test_3_actual>>;
+
+type Forest4 = [
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎅🏼', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+  ['🎄', '🎄', '🎄', '🎄'],
+];
+type test_4_actual = FindSanta<Forest4>;
+//   ^?
+type test_4_expected = [1, 2];
+type test_4 = Expect<Equal<test_4_expected, test_4_actual>>;
